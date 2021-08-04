8f7b920bcb7cc843d25693027ada608481a4ba19
congcuong207 <88388918+congcuong175@users.noreply.github.com>
2021-08-04 16:16:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tsvid_ff/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tsvid_ff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DatabaseReference data=FirebaseDatabase.getInstance().getReference();
+        data.child("Hoc sinh").setValue("Do Thanh Ton");
+    }
+}
