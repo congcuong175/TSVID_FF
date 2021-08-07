149aa574b80d5665fd224a8dec757f8bb77007c8
congcuong207 <88388918+congcuong175@users.noreply.github.com>
2021-08-07 16:09:23 +0700

    Cuong

--- app/src/main/java/com/example/tsvid_ff/Database/DBContext.java
+++ app/src/main/java/com/example/tsvid_ff/Database/DBContext.java
@@ -4,7 +4,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
+import com.example.tsvid_ff.Activity.RegisterActivity;
 import com.example.tsvid_ff.Entity.Account;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DBContext {
-
+    int kt;
     public ArrayList<Account> getDataAccount()
     {
         ArrayList<Account> accounts=new ArrayList<>();
@@ -50,18 +50,38 @@ public class DBContext {
         return accounts;
     }
 
-    public void addAccount(Account acc){
+    public int addAccount(Account acc){
         DatabaseReference db= FirebaseDatabase.getInstance().getReference();
-        db.child("Account").push().setValue(acc);
+        kt=0;
+        db.child("Account").push().setValue(acc, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete( DatabaseError error,  DatabaseReference ref) {
+                if(error ==null)
+                {
+                    kt=1;
+                }
+            }
+        });
+        return  kt;
     }
-    public void updateAccountpassword(String id,String newpassword){
+    public int updateAccountInfor(Account acc)
+    {
+        kt=1;
         DatabaseReference db= FirebaseDatabase.getInstance().getReference("Account");
         db.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded( DataSnapshot snapshot,  String previousChildName) {
-                if(snapshot.getValue(Account.class).getId().equalsIgnoreCase(id))
+                if(snapshot.getValue(Account.class).getId().equalsIgnoreCase(acc.getId()))
                 {
-                    db.child("password").setValue(newpassword);
+                    snapshot.getRef().child("name").setValue(acc.getName(), new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete( DatabaseError error,  DatabaseReference ref) {
+                            if(error==null)
+                            {
+                                kt=0;
+                            }
+                        }
+                    });
                 }
             }
 
@@ -85,15 +105,25 @@ public class DBContext {
 
             }
         });
+        return kt;
     }
-    public void deleteAccount(String id){
+    public int updateAccountpassword(String id,String newpassword){
+        kt=0;
         DatabaseReference db= FirebaseDatabase.getInstance().getReference("Account");
         db.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded( DataSnapshot snapshot,  String previousChildName) {
                 if(snapshot.getValue(Account.class).getId().equalsIgnoreCase(id))
                 {
-                   snapshot.getRef().removeValue();
+                    snapshot.getRef().child("password").setValue(newpassword, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete( DatabaseError error, DatabaseReference ref) {
+                            if(error==null)
+                            {
+                                kt=1;
+                            }
+                        }
+                    });
                 }
             }
 
@@ -117,5 +147,48 @@ public class DBContext {
 
             }
         });
+        return kt;
+    }
+    public int deleteAccount(String id){
+        kt=0;
+        DatabaseReference db= FirebaseDatabase.getInstance().getReference("Account");
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded( DataSnapshot snapshot,  String previousChildName) {
+                if(snapshot.getValue(Account.class).getId().equalsIgnoreCase(id))
+                {
+                   snapshot.getRef().removeValue(new DatabaseReference.CompletionListener() {
+                       @Override
+                       public void onComplete(DatabaseError error, DatabaseReference ref) {
+                           if(error==null)
+                           {
+                               kt=1;
+                           }
+                       }
+                   });
+                }
+            }
+
+            @Override
+            public void onChildChanged( DataSnapshot snapshot,  String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved( DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved( DataSnapshot snapshot,  String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled( DatabaseError error) {
+
+            }
+        });
+        return kt;
     }
 }
