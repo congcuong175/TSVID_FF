2496dc99e1a5a522444cbcf109dd52a856d154e4
congcuong175 <88388918+congcuong175@users.noreply.github.com>
2021-08-11 17:16:36 +0700

    hao

--- app/src/main/java/com/example/tsvid_ff/Home.java
+++ app/src/main/java/com/example/tsvid_ff/Home.java
@@ -1,9 +1,13 @@
 package com.example.tsvid_ff;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
@@ -11,11 +15,15 @@ import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 public class Home extends AppCompatActivity {
 
     MeowBottomNavigation meowBottomNavigation;
+    public Toolbar toolbar;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         meowBottomNavigation=findViewById(R.id.a1);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.tb);
         meowBottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_account_circle_24));
         meowBottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_assignment_24));
         meowBottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_search_24));
@@ -27,21 +35,47 @@ public class Home extends AppCompatActivity {
                 switch (item.getId()){
                     case 1:
                         fragment=new TTCN();
+                        toolbar.setTitle("QUẢNG LÝ CÁ NHÂN");
                         break;
                     case 2:
-                        fragment=new DichVu();break;
+                        fragment=new DichVu();
+                        toolbar.setTitle("DỊCH VỤ");
+                        break;
                     case 3:
-                        fragment=new TraCuu();break;
+                        fragment=new TraCuu();
+                        toolbar.setTitle("TRA CỨU THÔNG TIN");
+                        break;
                     case 4:
-                        fragment=new HoTro();break;
+                        fragment=new HoTro();
+                        toolbar.setTitle("TRUNG TÂM HỖ TRỢ");
+                        break;
                 }
                 loadfragmeny(fragment);
             }
         });
         meowBottomNavigation.show(1,true);
+        adads();
+        meowBottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+            }
+        });
+
     }
 
     private void loadfragmeny(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.f1,fragment).commit();
     }
+    public void adads() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_toc_24);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
 }
